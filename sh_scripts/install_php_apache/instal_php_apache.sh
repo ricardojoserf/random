54186f37b39dfa2cebfa349899662eec82abc3d3
Ricardo Ruiz <rjruizfdez@gmail.com>
2018-04-23 17:21:30 +0200

    Add files via upload

---
+++ sh_scripts/install_php_apache/instal_php_apache.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt-get -y install apache2
+sudo apt-get -y install php7.0 libapache2-mod-php7.0
+sudo apt-get -y install php7.0-mysql php7.0-mcrypt php7.0-curl php-all-dev php7.0-gd php-pear php-imagick php7.0-pspell php7.0-xmlrpc php7.0-mbstring -y
+sudo apt-get -y install php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache  php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php-gettext
---
+++ sh_scripts/install_sublime/install_sublime.sh
@@ -0,0 +1,5 @@
+#!/sh
+
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt-get install sublime-text
