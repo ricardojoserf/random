01f4ac30b3ea796849e174d05e0024b829ca8444
Ricardo Ruiz <ricardojruiz93@gmail.com>
2019-04-18 19:28:05 +0200

    Update install_sublime.sh

--- sh_scripts/install_sublime/install_sublime.sh
+++ sh_scripts/install_sublime/install_sublime.sh
@@ -1,11 +1,6 @@
 #!/sh
 
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
-
-##Option 2
-#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-#echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-#sudo apt-get update
-#sudo apt-get install sublime-text
